a28a8aabffad7cf34dbf141815292466de6e35b7
guk, mu <garuru7080@gmail.com>
2022-06-10 18:45:13 +0900

    new order

--- bot.py
+++ bot.py
@@ -3,7 +3,9 @@ import discord
 from discord.ext import commands
 from to import Token
 
+
 bot=commands.Bot(command_prefix='ehe')
+
 
 @bot.event
 async def on_ready():
@@ -15,14 +17,10 @@ async def on_ready():
 @bot.command(aliases=[''])
 async def ehe(ctx):
     await ctx.send('Ehe te NADAYO!!!!!')    
-@bot.command(aliases=[' ehe ehe'])
+@bot.command(aliases=['he'])
 async def stop_doing_that_shit(ctx):
-    await ctx.send('그만해라.')
-@bot.command(aliases=[' bwai'])
-async def stop_doing_that_shit(ctx):
-    await ctx.send(url='https://cdn.discordapp.com/attachments/857079818872553493/969222351612960798/unknown.png')
-@bot.command()
-async def 따라하기(ctx,*,text):
-    await ctx.send(text)
+    await ctx.send('https://tenor.com/view/nandayo-gif-18919523')
 bot.run(Token)
+    
+
 
--- to.py
+++ to.py
@@ -1,2 +1,2 @@
 #to.py - 토큰을 저장용
-Token="ODExOTEzMDY4Njg2MDgyMDg4.YC5G-A.b4JaAf6h4NuFuEnUVFgWTMWhiAE"
+Token="ODExOTEzMDY4Njg2MDgyMDg4.GAob6q.yfBtiv9w2I80E_fC1wJm-uFT4zqPnlVV3OiijM"
